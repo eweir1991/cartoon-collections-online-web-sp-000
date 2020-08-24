e1366481217b5e0144509698357f53f0f3a178da
Erik Weir <erik.weir@key.me>
2020-08-24 20:31:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,8 +35,18 @@ def long_planeteer_calls(array)
 end
 
 long_planeteer_calls(assorted_words)
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+#=================================================================
+def find_the_cheese(array)
+   cheese_types = ["cheddar", "gouda", "camembert"]
+   array.find do |type|
+     cheese_types.include?(type)
+   end
 end
+find_the_cheese(cheese_types)
+#=================================================================
+
+
+
+
+
+
