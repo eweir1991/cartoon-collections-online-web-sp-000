fd5f6585431d96cfd4fa4b8de3d724af87661c0c
Erik Weir <erik.weir@key.me>
2020-08-24 20:19:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,6 +33,8 @@ def long_planeteer_calls(array)
   end
 end
 
+long_planeteer_calls(call_screams)
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
